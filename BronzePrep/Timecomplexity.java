9b86634a7f3661f750b83bb6f9955b2f1c7a56cd
darkmooninja <darkmooninja3481@gmail.com>
2021-06-30 00:21:37 -0700

    Time Complexity

---
+++ BronzePrep/Timecomplexity.java
@@ -0,0 +1,19 @@
+package BronzePrep;
+
+public class Timecomplexity {
+    // O(f(n)) how many times an operation runs. O(1) means an operation ran one time. O(N) means an operation ran N times.
+    // O(F(n^2)) is bigger than O(f(n)) therefore O(f(n^2)) takes more time
+    // In an equation like 5*n it is O(f(n)) because N takes the most time out of all of them
+    // Big O notation calculates worst-case time complexity
+    // If there are 2 blocks or 2 seperate for loops with 2 different varable it is O(f(n^2 + m))
+    for (int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            // constant time code here
+        }
+    }
+    for (int i = 1; i <= m + 58834; i++) {
+        // more constant time code here
+    }
+    // N * logn = Mlogn
+    // 3n + 4nlogn = Logn
+}
